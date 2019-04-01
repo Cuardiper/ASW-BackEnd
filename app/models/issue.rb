9b3070e26ce52669034aea1b7b6b414178b669ef
danielesquina <danielesquina@gmail.com>
2019-04-01 12:04:10 +0000

    Issues #1 #10 done

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,5 +2,5 @@ class Issue < ApplicationRecord
     validates :title, presence: true, length: { maximum: 140 }
     validates :description, length: { maximum: 300 }
     belongs_to :creator, class_name: 'User'
-    belongs_to :assignee, class_name: 'User'
+    belongs_to :assignee, optional: true, class_name: 'User'
 end
