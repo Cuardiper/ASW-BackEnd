100d5266312ca23814103c8750520b605a1b0b02
danielesquina <danielesquina@gmail.com>
2019-04-11 09:55:49 +0000

    remove some migrations

--- db/migrate/20190409164323_votos.rb
+++
@@ -1,4 +0,0 @@
-class Votos < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/migrate/20190411084004_drop_votes.rb
+++
@@ -1,9 +0,0 @@
-class DropVotes < ActiveRecord::Migration[5.1]
-  def up
-    drop_table :votes
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
