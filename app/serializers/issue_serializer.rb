44061bb433ca1d2855b50262005ad2f1987abd3b
danielesquina <danielesquina@gmail.com>
2019-05-09 13:49:48 +0000

    issue links

---
+++ app/serializers/issue_serializer.rb
@@ -0,0 +1,21 @@
+class IssueSerializer < ActiveModel::Serializer
+  attributes :id, :title, :description, :type_issue, :priority, :status, :votes, :creator_id, :assignee_id, :created_at, :updated_at, :_links
+
+  def _links
+    if object.assignee_id != nil
+      links = {
+        self: { href: "/issues/#{object.id}" },
+        creator: {  id: object.creator_id, name: User.find(object.creator_id).name, href: "/users/#{object.creator_id}"},
+        assignee: {  id: object.assignee_id, name: User.find(object.assignee_id).name, href: "/users/#{object.assignee_id}"},
+        comments: { href: "/issues/#{object.id}/comments"}
+      }
+    else
+      links = {
+        self: { href: "/issues/#{object.id}" },
+        creator: { href: "/users/#{object.creator_id}", id: object.creator_id, name: User.find(object.creator_id).name },
+        assignee: { href: nil, id: nil, name: nil },
+        comments: { href: "/issues/#{object.id}/comments"}
+      }
+    end
+  end
+end
