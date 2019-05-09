44bca51a41a6859932a1631cad7f0cee9ff00af6
Alvaro Luque <alvaro_25_98@hotmail.com>
2019-05-09 13:29:19 +0000

    merge

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -84,15 +84,6 @@ class CommentsController < ApplicationController
       if(@user_aux.nil?)
         render json: { meta: {code: 401, error_message: "Unauthorized"}}
       else
-<<<<<<< HEAD
-          
-          request_parameters = JSON.parse(request.body.read.to_s)
-          text = request_parameters["text"]
-          if @comment.update(text: text)
-          render json: @comment, status: :updated
-          else
-            render json: @comment.errors, status: :unprocessable_entity
-=======
           if @comment.reporter_id == @user_aux.id
             request_parameters = JSON.parse(request.body.read.to_s)
             text = request_parameters["text"]
@@ -101,7 +92,6 @@ class CommentsController < ApplicationController
             else
               render json: @comment.errors, status: :unprocessable_entity
             end
->>>>>>> e1a367b5675ac85c88dc634a537c1c5a80a0bc5f
           end
       end
     else #no venimos de api
