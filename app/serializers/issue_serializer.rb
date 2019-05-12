f2dfba4d5502a61851c6c442829b72f56c50126f
danielesquina <danielesquina@gmail.com>
2019-05-12 19:35:46 +0000

    añadido voters y watchers a la issue

--- app/serializers/issue_serializer.rb
+++ app/serializers/issue_serializer.rb
@@ -1,5 +1,25 @@
 class IssueSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :type_issue, :priority, :status, :votes, :creator_id, :assignee_id, :created_at, :updated_at, :_links
+  attributes :id, :title, :description, :type_issue, :priority, :status, :votes, :watches, :creator_id, :assignee_id, :created_at, :updated_at, :voters, :watchers, :_links
+  
+  def watches
+    @issue = Issue.find(object.id)
+    @issue.watchers.count()
+  end
+  
+  def watchers
+    @issue = Issue.find(object.id)
+    @issue.watchers.pluck(:id, :name)
+  end
+  
+  def voters
+    @result = []
+    #@votos = Voto.where(:issue_id => object.id)
+    Voto.where(:issue_id => object.id).each do |v|
+      @aux = User.where(:id => v.user_id).pluck(:id, :name)
+      @result.push(@aux)
+    end
+    return @result
+  end
   
   def _links
     if object.assignee_id != nil
