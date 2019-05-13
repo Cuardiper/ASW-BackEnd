ec0577a61ff541ed0b30ac5543af0c3b3bff1cca
EC2 Default User <ec2-user@ip-172-31-43-204.us-east-2.compute.internal>
2019-05-13 15:46:54 +0000

    arreglo post issue5

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -109,7 +109,7 @@ class IssuesController < ApplicationController
           if assignee == 0
              @issue = Issue.create(title: title, description: details, type_issue: type, priority: priority, creator_id: @user_aux.id, status:"new")
           else
-            assignee_aux = User.where(uid: assignee).first
+            assignee_aux = User.where(uid: assignee)
             if(assignee_aux)
               @issue = Issue.create(title: title, description: details, type_issue: type, priority: priority, creator_id: @user_aux.id, assignee_id: assignee,status:"new")
             else
