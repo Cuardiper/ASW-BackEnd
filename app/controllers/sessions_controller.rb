a280f7dbb70059d6989a05953a09a22230a61662
EC2 Default User <ec2-user@ip-172-31-43-204.us-east-2.compute.internal>
2019-05-29 17:09:46 +0000

    iuneviunbvihjbijvb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,6 @@ class SessionsController < ApplicationController
   
   def create
     user = User.from_omniauth(request.env["omniauth.auth"])
-    userAux = User.where(email: "carles.farre@gmail.com")
-    userAux.update(oauth_token: "103078372808824005733")
     session[:user_id] = user.id
     #render json: user.oauth_token
     redirect_to issues_path
