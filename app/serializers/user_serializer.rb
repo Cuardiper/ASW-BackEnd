c6db0200317e9ccd3334bd8b17761e244888c24f
EC2 Default User <ec2-user@ip-172-31-43-204.us-east-2.compute.internal>
2019-05-29 16:51:35 +0000

    bwefijbiwueb

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :oauth_token, :id, :name, :email, :foto, :_links#, :issues
+  attributes :uid, :oauth_token, :id, :name, :email, :foto, :_links#, :issues
   
   def _links
     links = {
